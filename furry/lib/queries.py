fd80c2a0e703622274ecb1b503be4a3a62bf702c
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-23 13:56:25 +0000

    chore(ui): refactor session store

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c9b7662959c40fcbb7e92903bad7747 = () => {
-  return '2025-02-16T18:26:46Z';
+const node_39c08d221dfa4bc187f0fc471594f6bd = () => {
+  return '2025-02-23T13:56:25Z';
 };
