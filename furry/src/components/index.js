85abd2239aa581c89cd07b1420aa11f50aaef21e
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-07 00:03:26 +0000

    fix(core): fix auth tokens

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd04581bc87748658d50007074c9cfc5 = () => {
-  return '2025-02-27T19:49:48Z';
+const node_455522cf4b5f4d67aa520da0d0059cf6 = () => {
+  return '2025-03-07T00:03:26Z';
 };
