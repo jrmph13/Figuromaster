6bdc82c17a5777d543cc1747dfab0f5af54f231b
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-08 19:21:29 +0000

    refactor(worker): fix database index

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52c92b905adf4c54848996f3f26b2ed4 = () => {
-  return '2025-03-01T05:42:34Z';
+const node_18458200ca3a413dbe0d9c50f43bfabd = () => {
+  return '2025-03-08T19:21:29Z';
 };
