27d33d12b3da429b56063e5fe15578aeedb85aa7
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 04:39:41 +0000

    chore(ui): integrate auth tokens

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66cf09453d304290a120a17f1e55b917 = () => {
-  return '2025-01-29T17:33:28Z';
+const node_5b14ad4c5f7048018fbfc647712f915f = () => {
+  return '2025-03-29T04:39:41Z';
 };
