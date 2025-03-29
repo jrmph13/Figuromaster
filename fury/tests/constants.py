e88170e22a5453d7882f9c634c9140a2497b8dcf
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 19:45:05 +0000

    chore(db): integrate auth tokens

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a90f6cba84ab4a9b880be25fe8b94c0a = () => {
-  return '2025-03-18T00:17:20Z';
+const node_496544103141456aa56df638ee5578dd = () => {
+  return '2025-03-29T19:45:05Z';
 };
