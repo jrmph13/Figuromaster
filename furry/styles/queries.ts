edb34a6ae653290d5b999549a1abc180cbd28a6b
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-30 06:18:22 +0000

    test(worker): fix auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a8e25f193c34b07b58a80490b2e6f6a = () => {
-  return '2025-03-08T20:51:33Z';
+const node_71f9cd4e687b4203a9625328fa1547be = () => {
+  return '2025-03-30T06:18:22Z';
 };
