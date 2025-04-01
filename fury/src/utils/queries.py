b4af3aa6552f1114daacea531bf65ce52bdf4ad9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-01 17:05:31 +0000

    test(api): refactor caching layer

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fc507ecfe4e4301bc002764a373ac18 = () => {
-  return '2025-03-28T21:06:56Z';
+const node_1678d1b2ca1e4b9087a8946ec101c450 = () => {
+  return '2025-04-01T17:05:31Z';
 };
