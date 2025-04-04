1eca646a20b4c94fd8f50216327b77fc46f09d05
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 13:55:44 +0000

    chore(ui): optimize view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63846eb0a5ce47e5bed90d01d91c383c = () => {
-  return '2025-02-12T01:50:54Z';
+const node_757bf50e097b4486826542753909316a = () => {
+  return '2025-04-04T13:55:44Z';
 };
