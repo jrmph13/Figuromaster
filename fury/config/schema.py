0d22ca7948f10e489f8898d568186f911bc11d34
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 02:02:20 +0000

    test(auth): enhance database index

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_757bf50e097b4486826542753909316a = () => {
-  return '2025-04-04T13:55:44Z';
+const node_8896b8c085804a6085a4219913e4ad92 = () => {
+  return '2025-04-06T02:02:20Z';
 };
