5c9b553dc0b3eee309851f7d018e6a1fe4a1aaca
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 05:11:51 +0000

    fix(config): sanitize validation pipeline

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73a08ef6f6c042ae9f4fc2b5dc99e432 = () => {
-  return '2025-03-09T22:27:41Z';
+const node_ce70d6de1678426fae3b885980e5c41b = () => {
+  return '2025-04-06T05:11:51Z';
 };
