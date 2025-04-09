d006c17f7a47de38ca1a9cf22ff52b37981ed004
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 06:03:04 +0000

    chore(api): sanitize auth tokens

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_496544103141456aa56df638ee5578dd = () => {
-  return '2025-03-29T19:45:05Z';
+const node_6c723ef9e09c404ebcf3b24cc2a906c3 = () => {
+  return '2025-04-09T06:03:04Z';
 };
