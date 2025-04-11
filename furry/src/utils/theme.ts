147ad1e953120a06e13ab8fb1817a8dfb16f3778
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 17:53:12 +0000

    perf(config): enhance payload parser

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4ab77861e1349bba57ca4433c8c0086 = () => {
-  return '2025-04-06T17:40:34Z';
+const node_ec224883838a4fcbbbe786179c416b1f = () => {
+  return '2025-04-11T17:53:12Z';
 };
