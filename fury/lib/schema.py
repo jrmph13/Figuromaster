b4a937385f6c80d77669fe83d8048f4d3197d0d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 12:47:34 +0000

    refactor(utils): enhance auth tokens

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce70d6de1678426fae3b885980e5c41b = () => {
-  return '2025-04-06T05:11:51Z';
+const node_3b1ea912a01948eaa0408fc319b1741d = () => {
+  return '2025-04-14T12:47:34Z';
 };
