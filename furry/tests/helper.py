b678a89ff8f5f1a22ca8dbe9306c96d6dd3a9c7b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 18:26:31 +0000

    perf(ui): optimize session store

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53d111577c994fe7b8daea2592dd5e1e = () => {
-  return '2025-04-08T15:37:16Z';
+const node_4c92ee59e33b422f80cd67426bed182f = () => {
+  return '2025-04-14T18:26:31Z';
 };
