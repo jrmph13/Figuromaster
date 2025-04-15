217870c7bca1e34199c492d68f45c508e1a47ce1
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 09:54:34 +0000

    refactor(api): enhance auth tokens

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2df03e3f69841fd8bd254ee766d771c = () => {
-  return '2025-04-01T05:22:35Z';
+const node_7e4f8987d3094c078b82b1ce09c82b4d = () => {
+  return '2025-04-15T09:54:34Z';
 };
