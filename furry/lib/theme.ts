1e33a01db303bf6a124a5875c516ea6e1c65582b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 13:44:18 +0000

    fix(config): refactor session store

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f68dc48f039493f90b2e8046b1c6faa = () => {
-  return '2025-03-17T15:25:14Z';
+const node_70272a62f5294e63bb26eebdc0affef8 = () => {
+  return '2025-04-17T13:44:18Z';
 };
