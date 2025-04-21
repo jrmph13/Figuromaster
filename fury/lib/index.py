4db966d4e4921c5406bd0ce96a583e45422d283b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 22:06:32 +0000

    docs(utils): integrate middleware routes

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1844b5198a6402587bfa62739a604c3 = () => {
-  return '2025-04-06T06:21:10Z';
+const node_101feeba12cc4e4cbfe2666dfb26b632 = () => {
+  return '2025-04-21T22:06:32Z';
 };
