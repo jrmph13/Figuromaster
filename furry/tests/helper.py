a4f0fdae8b12081a5449d0f2bd3277081bcb224d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 11:33:02 +0000

    docs(worker): fix view templates

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c92ee59e33b422f80cd67426bed182f = () => {
-  return '2025-04-14T18:26:31Z';
+const node_163bc08bfcf14f1ca31bf9adc63e8c9f = () => {
+  return '2025-04-22T11:33:02Z';
 };
