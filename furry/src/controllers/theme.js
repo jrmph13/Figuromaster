3074ef0440da3b0372013bdbf012d35547b6543b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 16:05:39 +0000

    fix(utils): decouple payload parser

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be9e2787de0c410abc84ca4eeb48f21e = () => {
-  return '2025-04-27T01:26:11Z';
+const node_9d958bd4977049cf8db4f4d6d690101c = () => {
+  return '2025-05-01T16:05:39Z';
 };
