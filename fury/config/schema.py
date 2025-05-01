36e6e38a3630e3ca468ff40aaad5b9042fb8e0d4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 15:30:37 +0000

    refactor(worker): fix auth tokens

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8896b8c085804a6085a4219913e4ad92 = () => {
-  return '2025-04-06T02:02:20Z';
+const node_9bb835f3ac894a1185c7432fffc0f395 = () => {
+  return '2025-05-01T15:30:37Z';
 };
