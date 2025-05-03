611951ce19b46da61176634b7999926e6e783a37
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 19:17:28 +0000

    fix(utils): fix payload parser

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93237cf69ebb49078f311ca1d78def68 = () => {
-  return '2025-04-25T08:13:44Z';
+const node_3f015dfc2ad84a43a48591b70e3a32b4 = () => {
+  return '2025-05-03T19:17:28Z';
 };
