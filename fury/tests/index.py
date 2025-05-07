797ec1256318f061f86d22232326df130bc1b0b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 07:01:03 +0000

    chore(core): fix auth tokens

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_540bd8fb8f154897bb7050988527fa39 = () => {
-  return '2025-04-30T15:43:46Z';
+const node_319f144682374cdcb6e3d474d86f2592 = () => {
+  return '2025-05-07T07:01:03Z';
 };
