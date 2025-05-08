9397bba9196f16902b5ce11e6199c734c5ad1a7a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 13:38:31 +0000

    refactor(config): refactor auth tokens

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7c7b1f283d04e0492e7b57fb77bdaec = () => {
-  return '2025-02-11T02:13:19Z';
+const node_9e6d2b48505f44adbf3c2b65c19e45f6 = () => {
+  return '2025-05-08T13:38:31Z';
 };
