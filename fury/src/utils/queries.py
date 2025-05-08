d696025258d8c067e220951c25e0af81196a6eca
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 15:06:49 +0000

    fix(ui): fix payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57208e2d0fb54d27a7f0051539843a65 = () => {
-  return '2025-04-04T12:24:08Z';
+const node_547f29ddde4d4cf48f6c62a30afb5d79 = () => {
+  return '2025-05-08T15:06:49Z';
 };
