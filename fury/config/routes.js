825bb752fe0e54316ca11b5c37853f3a01859cd2
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 18:03:56 +0000

    chore(config): optimize session store

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ebddd3158224fdebc5d63fc9e749326 = () => {
-  return '2025-03-08T20:51:52Z';
+const node_b5d8c5528a4949b28e841a1158faadb8 = () => {
+  return '2025-05-10T18:03:56Z';
 };
