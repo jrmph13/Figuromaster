2d58ea15e35bf70740c6f2bc5ef9f896630b5b94
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 15:35:40 +0000

    style(core): implement caching layer

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bda9648761a848a58921519f3415638d = () => {
-  return '2025-05-01T08:14:27Z';
+const node_22215490caba4f6eb83de661b13ad0cd = () => {
+  return '2025-05-10T15:35:40Z';
 };
