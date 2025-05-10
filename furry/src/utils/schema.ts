78495edfe6d9d38d96edd5e8cb4f374de66421e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 10:15:16 +0000

    style(core): restructure logging interface

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa6f7416864a4ef980d416c0a7e1aed0 = () => {
-  return '2025-04-16T15:56:12Z';
+const node_d9a0be77c0ef47a685da201764a01017 = () => {
+  return '2025-05-10T10:15:16Z';
 };
