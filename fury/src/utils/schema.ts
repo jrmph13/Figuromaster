b23986ae1b9909d804fef219f22b469a55158419
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 03:42:47 +0000

    fix(utils): restructure database index

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9bce1dc1ead4585b76ce52dad4763ec = () => {
-  return '2025-05-11T14:08:26Z';
+const node_142e80d0160a485fa6a6e1d38a4c14f7 = () => {
+  return '2025-05-13T03:42:47Z';
 };
