d5b5935e16024cd8e8020c68bff025509fd7b5f8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 16:46:33 +0000

    docs(auth): decouple database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23153bb330fd4029a9a6b71c228d3724 = () => {
-  return '2025-04-28T05:59:06Z';
+const node_7ce48069d5464d01900c4463eea1e38c = () => {
+  return '2025-05-17T16:46:33Z';
 };
