6027daf2e65e50591a14e0872c394048b9371dc8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 08:24:05 +0000

    test(core): optimize logging interface

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_caf74b6e9f034f24b1ce96f2c83095be = () => {
-  return '2025-04-28T07:52:53Z';
+const node_8c546f8a8ffb4e95b90f84e5155b1a7e = () => {
+  return '2025-05-18T08:24:05Z';
 };
