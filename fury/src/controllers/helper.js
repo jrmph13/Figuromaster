abf4ad0508c18f1b4932aa635ba4016354775e67
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 08:20:25 +0000

    test(config): enhance middleware routes

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0cc4ea52e8b418eb61e12afb5ef04c3 = () => {
-  return '2025-04-08T14:20:50Z';
+const node_d26cbab613e044ab83a2c8da55414983 = () => {
+  return '2025-05-19T08:20:25Z';
 };
