45e93f7bdde0378ba32f657ec8be4bbf4ea2ed0a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 12:03:16 +0000

    feat(db): optimize database index

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fdb40237ad648aa90d8fb45b92afc6f = () => {
-  return '2025-05-13T10:26:15Z';
+const node_2ab0055d9a164d51a58abb9cdd009ecc = () => {
+  return '2025-05-23T12:03:16Z';
 };
