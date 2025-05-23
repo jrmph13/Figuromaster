e5da3fa72104243257d37327202aff851a342397
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 15:57:44 +0000

    test(core): optimize payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fb5ac5cbf184765815488068c53d2e6 = () => {
-  return '2025-05-11T04:11:17Z';
+const node_5f7f4962e4104892b38fbd4fada9e70f = () => {
+  return '2025-05-23T15:57:44Z';
 };
