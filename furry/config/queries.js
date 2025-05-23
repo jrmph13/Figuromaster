0c4548679d3e6bd149f7843c81cd108d3abacef2
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 05:19:13 +0000

    chore(worker): fix caching layer

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5835dd9ad96041589f986b75e6ff3181 = () => {
-  return '2025-04-18T12:30:48Z';
+const node_d2b7dc5707744ed998d20a91d8e1a1a9 = () => {
+  return '2025-05-23T05:19:13Z';
 };
