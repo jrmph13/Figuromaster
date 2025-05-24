113007f28a8a3b19cd3c1858a914434811093bda
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 21:38:14 +0000

    chore(utils): update logging interface

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ce58cd91aff4092ab709c965cedd227 = () => {
-  return '2025-05-04T14:55:03Z';
+const node_f9a503a3e9ee4cfabaec9612c5218f15 = () => {
+  return '2025-05-24T21:38:14Z';
 };
