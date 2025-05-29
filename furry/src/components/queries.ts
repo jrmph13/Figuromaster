2ff1073e4a873dd8a8caba902e52a6de604d1f12
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 15:33:54 +0000

    test(auth): fix middleware routes

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e26f62a01d449f8a7aaa2c1a33f41d9 = () => {
-  return '2025-05-25T03:50:26Z';
+const node_ad4e00744ea04a899554c1b3b6dd5201 = () => {
+  return '2025-05-29T15:33:54Z';
 };
