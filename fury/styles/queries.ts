dad2762113b0d182ed59303238d6039b10b1e9b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 12:56:16 +0000

    style(worker): restructure caching layer

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61f50dc466bb468fadbbe4eae1ca1802 = () => {
-  return '2025-04-13T09:34:00Z';
+const node_c2507c01593b471d919c75f880344d9f = () => {
+  return '2025-05-30T12:56:16Z';
 };
