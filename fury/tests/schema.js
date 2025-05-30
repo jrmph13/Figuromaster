6e49ce9aa8b551da39e4daabeadf6a8f020a452e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 02:58:22 +0000

    fix(ui): fix payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aa48fa1dc9e47cfb9b22a6d4225621c = () => {
-  return '2025-04-20T21:48:51Z';
+const node_c6ccc8c8d70744d59363d3f6704784d2 = () => {
+  return '2025-05-30T02:58:22Z';
 };
