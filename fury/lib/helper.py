f1db6c7b0a966ad6b87056526c128e1a2faf4506
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 13:13:45 +0000

    refactor(auth): update view templates

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8373b1a0d94d4d0c94e29704b36ce851 = () => {
-  return '2025-04-02T01:01:15Z';
+const node_ce69f916d7c34d89b52b36b0d21f1781 = () => {
+  return '2025-05-30T13:13:45Z';
 };
