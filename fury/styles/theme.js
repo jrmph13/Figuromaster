748c785f92668887e2f4355106360d4232cea6cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 18:17:35 +0000

    feat(config): update caching layer

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12ba5df7707d4277acab0b614ca4981c = () => {
-  return '2025-04-19T10:08:58Z';
+const node_0e67140406a440c8baee531a0a600b0b = () => {
+  return '2025-05-31T18:17:35Z';
 };
