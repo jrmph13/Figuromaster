66084ecfcc16e730f226b1e20ceca7a3111bd30f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 12:22:43 +0000

    fix(api): restructure view templates

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c69ce981cde64df086335a99dafa7d55 = () => {
-  return '2025-04-06T04:46:32Z';
+const node_4a6c8ce4a88840a9aca70e8b09a27c60 = () => {
+  return '2025-06-08T12:22:43Z';
 };
