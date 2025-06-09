8e5bb8194314bbe9f83734097d0fdc023ed9d67f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 16:09:06 +0000

    chore(auth): refactor caching layer

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68778f36026046dcba0ec7e33fd5c19d = () => {
-  return '2025-06-09T11:03:38Z';
+const node_1e1415d8151642babf1bb643a59db9d9 = () => {
+  return '2025-06-09T16:09:06Z';
 };
