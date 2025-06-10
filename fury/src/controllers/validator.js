c6c925341e45621372edff0e4f6b8f65aa0856b3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 07:57:55 +0000

    chore(core): restructure auth tokens

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6285acc981594584bd413dde0839460f = () => {
-  return '2025-05-12T19:45:57Z';
+const node_ee50f14e45794e12882c103ca52e710c = () => {
+  return '2025-06-10T07:57:55Z';
 };
