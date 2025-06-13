450d5c9386145c676c8ce0e99685ef44178ebffb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 21:06:48 +0000

    chore(core): sanitize auth tokens

--- furry/styles/validator.js
+++ furry/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_266e987a3e93437691e1aca645be81d3 = () => {
-  return '2025-05-31T02:32:23Z';
+const node_881f6984bd654c81ab182c3437b5f62c = () => {
+  return '2025-06-13T21:06:48Z';
 };
