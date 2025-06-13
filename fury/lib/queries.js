8b6a44c0b07795dc06e40d8a274b4a6cca646496
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 08:58:10 +0000

    test(config): restructure payload parser

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af9a99ee0d384814b3febc879a5860eb = () => {
-  return '2025-05-08T05:40:48Z';
+const node_d356ca90eecd453c81842387221b3116 = () => {
+  return '2025-06-13T08:58:10Z';
 };
