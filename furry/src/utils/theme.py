ac5a4b6bfd49245ffb0c0464c7f797ccd6e4f743
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 15:59:14 +0000

    feat(worker): restructure payload parser

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_642a28c280ce401a847de4eba513ccef = () => {
-  return '2025-04-26T14:30:49Z';
+const node_818847a12cae425891d72fc42b6b09de = () => {
+  return '2025-06-13T15:59:14Z';
 };
