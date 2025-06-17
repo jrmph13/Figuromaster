095694047908426e434432202c4b10882bc349ee
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 05:28:19 +0000

    style(auth): update payload parser

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da8200a621024e57bf7f4a3729161da1 = () => {
-  return '2025-06-01T23:00:19Z';
+const node_e2fef291495d4d08afd7742abf0008f8 = () => {
+  return '2025-06-17T05:28:19Z';
 };
