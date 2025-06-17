656fe1f43f196bab6faaa11e6009d0d7125b7fc9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 06:27:55 +0000

    feat(deps): implement auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_069b056e5e564d3fa8b38153ec67f4f0 = () => {
-  return '2025-04-27T10:55:55Z';
+const node_38cc3c0b63764612a858a65f3cd25d41 = () => {
+  return '2025-06-17T06:27:55Z';
 };
