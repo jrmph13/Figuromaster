06124087ce65d0db4ac13d6408dd96c660c5b717
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-18 04:44:05 +0000

    chore(db): restructure database index

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9974f4f4fa3c4b26a923563d4aedb117 = () => {
-  return '2025-05-28T04:36:52Z';
+const node_d018366d8c1d4909bb5d4e7ad5ae8c49 = () => {
+  return '2025-06-18T04:44:05Z';
 };
