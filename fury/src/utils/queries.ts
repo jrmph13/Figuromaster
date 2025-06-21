c744dadc75f3494411c06e5d8de84c3ba1b94b43
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 12:50:04 +0000

    docs(auth): integrate view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51bcd0e8cb0a42dbb9346258814f6907 = () => {
-  return '2025-06-14T18:44:43Z';
+const node_c856d37dbd90453999a47f726872ddd1 = () => {
+  return '2025-06-21T12:50:04Z';
 };
