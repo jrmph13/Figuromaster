1ff489c7d3662a1acc2e6ccd8798cc5fd7afd844
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 09:13:22 +0000

    chore(utils): fix payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d26cbab613e044ab83a2c8da55414983 = () => {
-  return '2025-05-19T08:20:25Z';
+const node_9f0bd247557c4b7fae4bdafa0fa5d3dc = () => {
+  return '2025-06-22T09:13:22Z';
 };
