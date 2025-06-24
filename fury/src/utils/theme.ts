99b30a6b53ae53809a2be2da337573e46037761f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 01:33:46 +0000

    refactor(auth): optimize database index

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_458cacca0a5e4f8d93f4f70fa0c420a7 = () => {
-  return '2025-06-24T12:01:18Z';
+const node_5db5552aba284aa9a01a511407648d56 = () => {
+  return '2025-06-24T01:33:46Z';
 };
