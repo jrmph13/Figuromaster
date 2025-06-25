e13ca231bde1bba1566c279219093249d139ba1d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 22:24:05 +0000

    chore(auth): sanitize payload parser

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd928db015e5424584839dea98e329d1 = () => {
-  return '2025-06-01T07:15:41Z';
+const node_f1f96bccab77465fa2273b37411d110c = () => {
+  return '2025-06-25T22:24:05Z';
 };
