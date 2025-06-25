6ad71c585ac3fc4628c70cc46f7bad28adc3f5f7
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 20:28:12 +0000

    feat(deps): refactor middleware routes

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6057d9bc2c63421aa98186af7da800ad = () => {
-  return '2025-04-02T04:21:33Z';
+const node_93ef7a38d62a44f889937610b3ef7cda = () => {
+  return '2025-06-25T20:28:12Z';
 };
