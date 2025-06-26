d194648a337fea3277e1a1c80a38809afbe10ae0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 18:29:02 +0000

    docs(deps): decouple payload parser

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_995dce69507a487c81a213f5de62557b = () => {
-  return '2025-06-09T05:00:15Z';
+const node_3818d390bfdd426795ddc07c12099c82 = () => {
+  return '2025-06-26T18:29:02Z';
 };
