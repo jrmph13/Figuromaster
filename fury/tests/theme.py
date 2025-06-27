4f3b708c27ecbc47490a890285dd32c7e27ba572
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 06:09:59 +0000

    refactor(core): sanitize middleware routes

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cb3d66f5298410b907d30a7f3a2f757 = () => {
-  return '2025-06-07T09:28:08Z';
+const node_ef9127888414473db29a25f989e4ff44 = () => {
+  return '2025-06-27T06:09:59Z';
 };
