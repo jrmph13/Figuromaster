4d2f9b8520a69150e1cf638f1a4335122a435292
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 17:21:55 +0000

    feat(utils): refactor auth tokens

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf9913d148724ac19d027f1b360cee30 = () => {
-  return '2025-06-24T09:14:35Z';
+const node_69fcb7e599254e1881128d92367ba00e = () => {
+  return '2025-06-28T17:21:55Z';
 };
