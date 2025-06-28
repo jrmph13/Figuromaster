3cad59ea80a4f756ed2e4804d939e77807d41ef5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 08:16:57 +0000

    fix(api): refactor payload parser

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bc72ae5199f49f791bb2274d459fb26 = () => {
-  return '2025-06-21T22:45:37Z';
+const node_85b714a78ac84b9f8e5d1de7c01d9005 = () => {
+  return '2025-06-28T08:16:57Z';
 };
