212fd033186e8008b89eca3929cc4a2b5d77cfb4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 21:05:52 +0000

    feat(utils): fix view templates

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d1348434d3d4b58a5e0653abb9e5877 = () => {
-  return '2025-06-07T19:50:55Z';
+const node_586fb0a9053f41b39dce8990368cff3f = () => {
+  return '2025-06-28T21:05:52Z';
 };
