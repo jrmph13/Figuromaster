10ccb4cb7243dcb7eccd0b598ecc6f9ca6e90efa
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 03:38:59 +0000

    style(api): restructure view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ed6a94c9e984d61884ef075df5ba608 = () => {
-  return '2025-06-16T08:21:03Z';
+const node_1eccc3f4b4404cf891adc807ea645919 = () => {
+  return '2025-06-30T03:38:59Z';
 };
