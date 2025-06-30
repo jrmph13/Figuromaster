e504cb05c6dd9ed848a1d6b4b84f327e76ed7a0f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 02:44:32 +0000

    style(db): decouple middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c87877d476de4e1db09f012ef3647b6c = () => {
-  return '2025-02-13T08:55:28Z';
+const node_8192b8e520cf419bbd228e5bc73b8b97 = () => {
+  return '2025-06-30T02:44:32Z';
 };
