3e9e2575ca3d7d11278442848fbcec9125eee21c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 06:39:49 +0000

    test(auth): restructure middleware routes

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b17d08b4a8d34ab59cae8e670ffd0ca9 = () => {
-  return '2025-06-18T11:07:23Z';
+const node_4b5a941d8cb24b4fa93a1bfe7f64a889 = () => {
+  return '2025-07-01T06:39:49Z';
 };
