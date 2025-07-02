2429948e5272c5d29d666f7c3bc54b1de8a246d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 22:51:42 +0000

    perf(config): enhance payload parser

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0da04f7397454e2f9220f029d21b8136 = () => {
-  return '2025-04-26T06:43:12Z';
+const node_3846ca8b4e9f45d2922df91216ceffcd = () => {
+  return '2025-07-02T22:51:42Z';
 };
