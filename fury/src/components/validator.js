2be24554fc0df1ccc5f5a02fca52a05348b3671e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 13:14:07 +0000

    feat(worker): update auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c17f92887c0a471790cf0fc613ec0693 = () => {
-  return '2025-06-14T15:13:12Z';
+const node_f6390d874b8b4d29b99210f57968ecc0 = () => {
+  return '2025-07-05T13:14:07Z';
 };
