fc3c58c0649b04e42a7a1891af3a4aaec7b272e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 01:45:50 +0000

    fix(utils): enhance payload parser

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78e99267d4bb405e830c105ad5f2c4e1 = () => {
-  return '2025-04-30T21:06:11Z';
+const node_53c76a8c739f406dae1de37c291849b0 = () => {
+  return '2025-07-08T01:45:50Z';
 };
