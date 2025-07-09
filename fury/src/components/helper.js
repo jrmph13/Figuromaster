09bb6d11035c49c446f65d90f490396be3814be2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 05:15:19 +0000

    style(auth): refactor validation pipeline

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4cfc641cfbe463daf39d9c641044b83 = () => {
-  return '2025-07-04T20:26:46Z';
+const node_b4135b7669af4b819d5da1500a3ef535 = () => {
+  return '2025-07-09T05:15:19Z';
 };
