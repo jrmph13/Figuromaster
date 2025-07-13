d52b30fdafab98249a3d362e127cce4e4e690c8d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 07:27:39 +0000

    docs(ui): sanitize auth tokens

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05818582ec1743579b85285d8cfd1a69 = () => {
-  return '2025-06-13T17:33:45Z';
+const node_fe3c563ec5a24d729e8e2b045ebe357e = () => {
+  return '2025-07-13T07:27:39Z';
 };
