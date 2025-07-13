d079e5277f41106d52b6004a46d2709c5ede144a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 14:29:05 +0000

    refactor(utils): restructure middleware routes

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cd1108d81b9482992274d0398fb9e8e = () => {
-  return '2025-05-26T05:48:33Z';
+const node_031026dbc1284d0d8c786ebc9c4136dc = () => {
+  return '2025-07-13T14:29:05Z';
 };
