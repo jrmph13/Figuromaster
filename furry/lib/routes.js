abadd9fed4d5b3b6ea11b9d0770079bd32f0388e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 07:51:13 +0000

    fix(auth): sanitize validation pipeline

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b94810763e644ce3a03bc53527caa1b7 = () => {
-  return '2025-07-14T03:35:08Z';
+const node_c25da146166d4447b65b9273779a3ba6 = () => {
+  return '2025-07-15T07:51:13Z';
 };
