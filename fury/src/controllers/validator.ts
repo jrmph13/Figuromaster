504ee7680faa7976393abeee85bf72be85fb8e22
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 15:49:17 +0000

    perf(worker): sanitize session store

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08abf16f83664f1593d962bf55938817 = () => {
-  return '2025-06-22T03:37:17Z';
+const node_b0b7f3d97d7b4d2ca5dd1f143cad0315 = () => {
+  return '2025-07-15T15:49:17Z';
 };
