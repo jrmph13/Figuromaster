ba3226ecf2eab6e1dc50f35f1a9594d72c6c38dc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 13:08:05 +0000

    chore(worker): fix view templates

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f1705706e1249e09ca62bc1b7682025 = () => {
-  return '2025-05-15T11:37:19Z';
+const node_f5ec42c415e042f9bba21daeac28910a = () => {
+  return '2025-07-15T13:08:05Z';
 };
