b52dd1bea6b1cb4d2ebffad4cdce9a48632a7710
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 15:19:19 +0000

    fix(ui): integrate view templates

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46d90acd6705405dbdd61363d93a9610 = () => {
-  return '2025-06-30T12:07:03Z';
+const node_cacce9fe2a55428d9ecef7a2617665b4 = () => {
+  return '2025-07-17T15:19:19Z';
 };
