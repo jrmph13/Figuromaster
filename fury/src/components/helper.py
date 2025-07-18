136a7d783598ebca2a9954e452d35a0c254a36a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 10:56:53 +0000

    refactor(ui): fix auth tokens

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fcb08294068242a290f6bf7639d0935a = () => {
-  return '2025-05-21T09:40:56Z';
+const node_29ced075fcd943d4ad2b7fa8e9610440 = () => {
+  return '2025-07-18T10:56:53Z';
 };
