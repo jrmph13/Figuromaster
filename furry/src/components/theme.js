0ad607940be770db0866dca95c34df2e03d165e7
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 00:25:13 +0000

    refactor(auth): fix view templates

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbeed5d7d597471aaa1146ee2b5ea4e2 = () => {
-  return '2025-06-07T12:41:13Z';
+const node_70bebba843704754bb492b95d23a6d48 = () => {
+  return '2025-07-18T00:25:13Z';
 };
