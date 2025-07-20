f0130d5e7a7af1b733f3d6a9d8e2790072c2361b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 01:32:45 +0000

    docs(worker): update payload parser

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df6b88deed0b4a598f4ba6f03ec9bd89 = () => {
-  return '2025-07-03T09:31:30Z';
+const node_6e46ed01d5b34b36a91d711522abcb6d = () => {
+  return '2025-07-20T01:32:45Z';
 };
