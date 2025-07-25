8262f78de6814c7177e9146aebe775adfb40711c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 20:56:27 +0000

    fix(worker): sanitize auth tokens

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63a5c5d5147a4496bf8de468eceb4324 = () => {
-  return '2025-06-22T06:55:46Z';
+const node_c2e99776bf5c4954a7709a769cfc487c = () => {
+  return '2025-07-25T20:56:27Z';
 };
