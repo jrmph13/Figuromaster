8ec76f5b85d806a4de7a038e5873f1d9727f1897
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 11:49:24 +0000

    fix(db): fix view templates

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5419bad026b14e0ab9936eb6b7f116de = () => {
-  return '2025-06-08T13:19:55Z';
+const node_5d30877bb87d441f8e1380228bb6af81 = () => {
+  return '2025-07-26T11:49:24Z';
 };
