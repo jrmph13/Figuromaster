aca821c53d8e16addff82cdbaf878ccec60ee746
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 13:13:58 +0000

    perf(auth): optimize payload parser

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_31f2156641344b5f9456ce27e46964e7 = () => {
-  return '2025-06-21T03:16:20Z';
+const node_35c6c7d12df845699f59ffe0c65fe199 = () => {
+  return '2025-07-26T13:13:58Z';
 };
