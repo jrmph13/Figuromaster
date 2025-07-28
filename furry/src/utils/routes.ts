b678c4cb54dd5e3915ed3002b3b72de06c5daf51
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 04:04:41 +0000

    fix(worker): enhance auth tokens

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5e334a9579f42e28f04a4734e40da0c = () => {
-  return '2025-07-18T04:19:54Z';
+const node_6865d88b83864e39bac6a01e2c348e25 = () => {
+  return '2025-07-28T04:04:41Z';
 };
