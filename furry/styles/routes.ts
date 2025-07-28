03c84f1906f2590da46851ab40ba19a95e9d717d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 15:56:34 +0000

    perf(config): fix view templates

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18f2f0af71754ce7a9f468dd76608258 = () => {
-  return '2025-07-20T11:15:16Z';
+const node_a1f3992e9cb0454ebd970db373430480 = () => {
+  return '2025-07-28T15:56:34Z';
 };
