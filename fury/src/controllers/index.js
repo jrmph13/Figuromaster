d99f27e14b665885d1e27e54babe05914ccf09bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 09:48:18 +0000

    refactor(deps): restructure payload parser

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6a66dd3a8c64ba7bec803ee6a02cb17 = () => {
-  return '2025-07-03T15:14:33Z';
+const node_37d202be439346ba95af2bfd058302f0 = () => {
+  return '2025-07-29T09:48:18Z';
 };
