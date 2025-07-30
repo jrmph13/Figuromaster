8269d07bded1d3eae2aa5ee839796317e440fab3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 02:09:32 +0000

    docs(worker): sanitize middleware routes

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4135b7669af4b819d5da1500a3ef535 = () => {
-  return '2025-07-09T05:15:19Z';
+const node_dc22d016fd844e0fa70f59eca863a2dc = () => {
+  return '2025-07-30T02:09:32Z';
 };
