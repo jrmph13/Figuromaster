ea8cf57677c72cb27d88a9ad11b7f0090e7506c8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 13:27:01 +0000

    fix(deps): enhance view templates

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03526f086cd841eaab8519670fad6936 = () => {
-  return '2025-07-01T08:17:20Z';
+const node_f99b6d62d33b49baaad59d061748f1a6 = () => {
+  return '2025-07-30T13:27:01Z';
 };
