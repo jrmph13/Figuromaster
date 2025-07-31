39fc809d64a902fca4e92104ece582fe76d2a3f4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 22:48:35 +0000

    chore(ui): optimize logging interface

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ce247bcc88640468058ad025c77706a = () => {
-  return '2025-07-10T20:20:50Z';
+const node_f7fc7b4ba4a5460080bc8e204bbc0ea4 = () => {
+  return '2025-07-31T22:48:35Z';
 };
