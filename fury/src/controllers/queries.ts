802e8b1022aae2ff4841addab7152a47a261d380
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 17:35:41 +0000

    docs(worker): sanitize auth tokens

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ea2d7e85a954d24b1da1b6a074c1ef8 = () => {
-  return '2025-06-22T11:14:21Z';
+const node_706232528631461d846b9c540ac5d6e0 = () => {
+  return '2025-08-01T17:35:41Z';
 };
