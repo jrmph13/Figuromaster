8cd4f6d9a921e020122b650707da53491874a170
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 10:59:30 +0000

    docs(worker): enhance view templates

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_557cef2b2c8d4299aa98e6b986a32317 = () => {
-  return '2025-07-11T16:55:24Z';
+const node_1b354071d1a245d9a6027385f7b93f6b = () => {
+  return '2025-08-03T10:59:30Z';
 };
