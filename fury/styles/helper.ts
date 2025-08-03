c3ca72cefacb8e6b16fb892197ac5a3b308c604f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 02:39:44 +0000

    refactor(db): implement payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_044f7d425b064fe4962b329f919e91ec = () => {
-  return '2025-04-22T06:26:26Z';
+const node_9837edb7bb144ee299d4686fa6d11399 = () => {
+  return '2025-08-03T02:39:44Z';
 };
