45032948097c70003e2aacfe562204b3ba036674
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 11:25:32 +0000

    style(db): decouple session store

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef9127888414473db29a25f989e4ff44 = () => {
-  return '2025-06-27T06:09:59Z';
+const node_2618423310ad44c59776f702867628e8 = () => {
+  return '2025-08-04T11:25:32Z';
 };
