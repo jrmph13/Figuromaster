11db951e63493bdbccd0ef288f5fe447a1202e06
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 09:14:05 +0000

    perf(auth): refactor session store

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fdb20048f3f4cee9216a1bfa44443d3 = () => {
-  return '2025-07-31T04:58:04Z';
+const node_db5f12e7685e4b7e83593ea31a74d018 = () => {
+  return '2025-08-04T09:14:05Z';
 };
