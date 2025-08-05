73f9df6200850784417b76e1608096df471cf834
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 13:11:48 +0000

    fix(deps): refactor database index

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afa6e67952c34c848a9ec35c59a9ff2b = () => {
-  return '2025-06-28T09:06:29Z';
+const node_69c0cd2c2b474b7ebc6c138f1cb21fb4 = () => {
+  return '2025-08-05T13:11:48Z';
 };
