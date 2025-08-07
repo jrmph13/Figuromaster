fee1bcb3287fe1cc704a1b1b1dcaa163b3c53b91
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 17:50:45 +0000

    chore(worker): sanitize middleware routes

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e168af22ac549ea927fa5e396083f4d = () => {
-  return '2025-08-06T01:28:04Z';
+const node_305cbe92a7dd43dcb6294c32f0de6ea3 = () => {
+  return '2025-08-07T17:50:45Z';
 };
