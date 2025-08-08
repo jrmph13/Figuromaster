5f9809a8fda9daba9350fda4af119dc94ff97347
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 21:51:19 +0000

    chore(core): restructure middleware routes

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8b58e67287a4857ae5fd3b922e2cc5e = () => {
-  return '2025-07-09T02:40:02Z';
+const node_fd0ac20b9be94c7387c37b25ef55fe67 = () => {
+  return '2025-08-08T21:51:19Z';
 };
