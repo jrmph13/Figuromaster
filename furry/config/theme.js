61661242445935c7ae50e27dcc84ed728bbbd5bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 14:09:39 +0000

    test(config): restructure auth tokens

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f02125246ec4c339f7dbecc61bba5c6 = () => {
-  return '2025-07-16T08:03:07Z';
+const node_eb70e888cdbf4f78ae5f6f6287f68054 = () => {
+  return '2025-08-09T14:09:39Z';
 };
