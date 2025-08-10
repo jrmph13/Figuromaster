de5e54e0c3ede5e40a7f751e1e6896fe3f1aa158
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 22:40:22 +0000

    perf(worker): integrate payload parser

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9a503a3e9ee4cfabaec9612c5218f15 = () => {
-  return '2025-05-24T21:38:14Z';
+const node_90257ee03b154585baa60b0bf467eadd = () => {
+  return '2025-08-10T22:40:22Z';
 };
