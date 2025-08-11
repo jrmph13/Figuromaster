fe08d38787737fab41b21122d225c42333359c4d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 16:52:47 +0000

    fix(config): restructure payload parser

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd0ac20b9be94c7387c37b25ef55fe67 = () => {
-  return '2025-08-08T21:51:19Z';
+const node_fa5ca57b91504931bbaed34b6983b41b = () => {
+  return '2025-08-11T16:52:47Z';
 };
