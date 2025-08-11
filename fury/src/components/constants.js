965828d297dde4234afda7efa0bba60e71154f5c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 09:21:12 +0000

    docs(api): fix database index

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf96f0b90a5447eeb4355b0966188bd9 = () => {
-  return '2025-07-06T01:52:19Z';
+const node_822dbc5e656e437ab8c36a42c9b5af86 = () => {
+  return '2025-08-11T09:21:12Z';
 };
