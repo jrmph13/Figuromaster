f13ed41ca4e2390bd35af441f5a3cc531b432993
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 01:30:34 +0000

    feat(config): update logging interface

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2618423310ad44c59776f702867628e8 = () => {
-  return '2025-08-04T11:25:32Z';
+const node_51f2c10cbd9042b393323459dcf2cab6 = () => {
+  return '2025-08-11T01:30:34Z';
 };
