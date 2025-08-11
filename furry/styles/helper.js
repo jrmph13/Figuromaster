a166341588537b55812ea5e450ed66bc94633c06
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 16:54:35 +0000

    perf(ui): fix payload parser

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d01e9d9d7f824b59ba2751411c57f470 = () => {
-  return '2025-06-13T10:08:40Z';
+const node_4912197ae6914d8c8d47e6853cca9272 = () => {
+  return '2025-08-11T16:54:35Z';
 };
