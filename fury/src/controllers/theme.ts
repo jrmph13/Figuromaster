ca258d65adb2d90956e2b4f54345d0cf9eb569d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 19:43:21 +0000

    test(worker): sanitize session store

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0247d5ff1a284f56b196ba7a785f8e80 = () => {
-  return '2025-08-08T15:37:22Z';
+const node_e9aa290518ea4ebbb316b552e5e0d2cc = () => {
+  return '2025-08-14T19:43:21Z';
 };
