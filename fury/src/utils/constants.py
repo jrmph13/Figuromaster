df2536a7aa90ce81180901797d1e2b123d21891d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 01:02:11 +0000

    fix(worker): decouple view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6a3c13abe154852a5d857855246673c = () => {
-  return '2025-07-28T21:11:21Z';
+const node_3cb9a3d19d7f4272a1fc1e494ef0c651 = () => {
+  return '2025-08-19T01:02:11Z';
 };
