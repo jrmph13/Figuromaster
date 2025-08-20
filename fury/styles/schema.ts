6109c666128cd0fa9d29f8eb48134f3882d17aa1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 10:11:23 +0000

    style(db): refactor caching layer

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9176d32bed1b4d83ade07595854db5a0 = () => {
-  return '2025-07-04T15:39:29Z';
+const node_f564706e3e97414d8870617e874f9748 = () => {
+  return '2025-08-20T10:11:23Z';
 };
