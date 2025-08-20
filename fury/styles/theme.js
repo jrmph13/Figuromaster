09317f4886d06c040f2f7c4cd1b3511f73663908
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 12:45:55 +0000

    perf(core): integrate view templates

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a10d06c193304d0ab3c504c693c50651 = () => {
-  return '2025-08-04T13:02:02Z';
+const node_8677f47c052f467bb5a1a2664ec5cac1 = () => {
+  return '2025-08-20T12:45:55Z';
 };
