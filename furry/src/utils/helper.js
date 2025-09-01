09c19ba5d3a215f517de6e6dcc3f3b6bbe5ac15a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-01 08:35:32 +0000

    chore(deps): fix logging interface

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e9e6c1941d347e783f09838a223e6f4 = () => {
-  return '2025-06-22T08:12:59Z';
+const node_4f5a183567784c76b192c79ec5703fd2 = () => {
+  return '2025-09-01T08:35:32Z';
 };
