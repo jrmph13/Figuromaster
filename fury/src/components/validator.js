b4e5e6936b0ff31228f98c9480d14a7883be6d12
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-06 15:54:38 +0000

    test(core): integrate payload parser

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9bcb1252aeda43f5a259c86481547e7d = () => {
-  return '2025-07-15T13:18:07Z';
+const node_e5ed8e686c64470ca8c3c474d7422385 = () => {
+  return '2025-09-06T15:54:38Z';
 };
