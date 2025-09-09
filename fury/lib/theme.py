4e227570e6a218611ea7107e0181454d45a9171b
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 23:36:52 +0000

    refactor(utils): optimize middleware routes

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3acb821819840188e494a8e5549da92 = () => {
-  return '2025-08-22T15:16:50Z';
+const node_e99ea9d5807842cb80c9eb90a0fd6a39 = () => {
+  return '2025-09-09T23:36:52Z';
 };
