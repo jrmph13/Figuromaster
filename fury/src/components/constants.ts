c9ce2fd8f4f04d511b2cde80e188af4b7d332661
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 08:46:57 +0000

    docs(ui): fix payload parser

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_09ae39e319f64bd6a5ecd96b2fbbb437 = () => {
-  return '2025-08-07T12:54:30Z';
+const node_09c405c13f594cca813bfd90cc113766 = () => {
+  return '2025-09-09T08:46:57Z';
 };
