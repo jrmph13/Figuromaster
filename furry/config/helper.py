966cc2d01507c0babff31acbb89bacde3fe8def2
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 23:40:30 +0000

    feat(auth): restructure validation pipeline

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f74e44a7ad0e4441abcf30d173d8e4d6 = () => {
-  return '2025-07-31T12:14:41Z';
+const node_ff14c862c9d347d09bade3e211c8ce16 = () => {
+  return '2025-09-09T23:40:30Z';
 };
