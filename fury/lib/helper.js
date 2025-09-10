78b565d0a4fe04cff4218c1156b8093ce2ab859a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-10 12:50:17 +0000

    feat(core): enhance payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04b8fd09819e4ad4aac7c5e959bd86c8 = () => {
-  return '2025-05-07T18:13:01Z';
+const node_d8558445b5414666a1fdfd3191d2c340 = () => {
+  return '2025-09-10T12:50:17Z';
 };
