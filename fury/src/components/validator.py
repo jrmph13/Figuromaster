3e6e7a8f99d3c26e912cf103ec24a30800dd4333
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-15 11:00:01 +0000

    perf(auth): fix payload parser

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b1848679b504ead9b158ab665e70d7a = () => {
-  return '2025-07-15T14:52:57Z';
+const node_7bba5331b90e4ddd8ef4fe6bd0fd0d19 = () => {
+  return '2025-09-15T11:00:01Z';
 };
