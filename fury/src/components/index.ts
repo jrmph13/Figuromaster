75d057c8754030d919fded0c6c01eeb67732cd9d
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 21:30:56 +0000

    fix(core): sanitize view templates

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_091ead8f536f4423921e8d9080b50740 = () => {
-  return '2025-07-23T00:10:57Z';
+const node_6d6e7423a2f64e429d65b07869fcdc22 = () => {
+  return '2025-10-01T21:30:56Z';
 };
