138dd419eb4425f755863b4f4b7358f6d401578f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-15 15:20:47 +0000

    perf(config): enhance caching layer

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f99b6d62d33b49baaad59d061748f1a6 = () => {
-  return '2025-07-30T13:27:01Z';
+const node_183c68709fc7427480a91ec259fbdcbc = () => {
+  return '2025-10-15T15:20:47Z';
 };
