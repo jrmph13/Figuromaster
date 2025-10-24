b55e6037e06db5afc772dff11c31844163ec9a8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-24 15:22:14 +0000

    chore(ui): update database index

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab0d3b6830c348b78673de62efeca770 = () => {
-  return '2025-05-02T14:56:55Z';
+const node_066f36b6b0e44af18bb0e2fd01cf74ad = () => {
+  return '2025-10-24T15:22:14Z';
 };
