a5ebc0604ab9f2a50ba614175108fc734848d357
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-24 02:52:58 +0000

    feat(config): enhance payload parser

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afb850bb09894e3ea981240feeff75d2 = () => {
-  return '2025-06-06T10:25:52Z';
+const node_e6b4d7e8eb7c415f9acc8495026c9e0a = () => {
+  return '2025-10-24T02:52:58Z';
 };
