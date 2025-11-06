33dde0f507cb5b23c2d63c711438ff960a11fc9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-06 03:02:44 +0000

    perf(deps): implement payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb01c7d74e80421799d98bc4c0f2f33c = () => {
-  return '2025-08-02T04:59:49Z';
+const node_3f10410ae5e44ae59d8f3407c2d20305 = () => {
+  return '2025-11-06T03:02:44Z';
 };
