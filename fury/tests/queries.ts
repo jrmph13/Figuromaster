5ecb14a63d5d4b258dc604f0bd10d7c13f1e7dbb
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-06 07:44:49 +0000

    refactor(utils): enhance middleware routes

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_412821cfda4045a28883f6b06d16e63f = () => {
-  return '2025-08-12T05:14:14Z';
+const node_efa0ec2ad0bd409fb91c5fe1a8932f4d = () => {
+  return '2025-11-06T07:44:49Z';
 };
