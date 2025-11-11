4e60201e780b65b24cf123c08c1d764f8a29ec1c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 09:28:43 +0000

    feat(db): fix view templates

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9647a237d7384141b04d791633600e36 = () => {
-  return '2025-07-31T19:19:13Z';
+const node_9532aa7ba0534e15b8bd2e069afd9fd4 = () => {
+  return '2025-11-11T09:28:43Z';
 };
