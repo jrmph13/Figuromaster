3a96f585d8585c18d8077e13f43d71d12224a2f2
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 14:04:33 +0000

    perf(api): integrate middleware routes

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b229528f2a04160ab19f3d1a8e0775f = () => {
-  return '2025-10-26T02:29:48Z';
+const node_0e3d779ddcc1465c80679accf8c7679f = () => {
+  return '2025-11-12T14:04:33Z';
 };
