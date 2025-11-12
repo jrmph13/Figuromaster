68b7839e9d488fc60d1aa8d26957c9cb05d7d630
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 10:28:03 +0000

    perf(db): enhance database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58dac0ca161444ca9b01d8dc53145b7b = () => {
-  return '2025-08-07T02:31:23Z';
+const node_1b6d1039fe844a12befc6e912e6043fd = () => {
+  return '2025-11-12T10:28:03Z';
 };
