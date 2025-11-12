bfc41a9f4ac9971df3eafd3a4447f2dc4c1d1a94
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 00:52:41 +0000

    refactor(utils): fix middleware routes

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5195fa302ac45dfa2a2166077dde71a = () => {
-  return '2025-06-12T22:29:53Z';
+const node_48c85f1475c04c128a01f19467d1aa73 = () => {
+  return '2025-11-12T00:52:41Z';
 };
