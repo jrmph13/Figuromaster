b6f31e883b1edcadd93e38a47d477126b913081f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 21:34:11 +0000

    style(auth): sanitize session store

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1221daf6a99419da4796134b1b4b4d0 = () => {
-  return '2025-06-27T12:02:49Z';
+const node_9dfd0d7debd1437e893c89e01baa0796 = () => {
+  return '2025-11-18T21:34:11Z';
 };
