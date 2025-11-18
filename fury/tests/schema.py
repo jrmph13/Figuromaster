4368d470baae250ac54b1cf5c972edf1c84b887e
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 14:35:12 +0000

    chore(auth): update view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0be50583602a4e5c98cde974d2304d86 = () => {
-  return '2025-09-04T23:52:30Z';
+const node_b4716777853c419da2fd9a9a8d3374c8 = () => {
+  return '2025-11-18T14:35:12Z';
 };
