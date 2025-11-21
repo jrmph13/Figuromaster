ac639f38d7a46a0c556ab6efa45b2d1b5777ab43
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-21 13:09:48 +0000

    docs(ui): update payload parser

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8192b8e520cf419bbd228e5bc73b8b97 = () => {
-  return '2025-06-30T02:44:32Z';
+const node_c038108b928d41c39a62ec60f014ed95 = () => {
+  return '2025-11-21T13:09:48Z';
 };
