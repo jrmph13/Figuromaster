043db0ab48c728e10cb27035fbcb513777913e9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 15:53:46 +0000

    perf(auth): optimize caching layer

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_221da6197b4f491f839e89e20608ea7f = () => {
-  return '2025-08-05T02:19:09Z';
+const node_7902298133ad45aab452767cfc8f8518 = () => {
+  return '2025-11-24T15:53:46Z';
 };
