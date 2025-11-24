ee7bf61334ff7caba729ebbc82d1951887fe7204
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 03:07:13 +0000

    fix(core): restructure session store

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7efe96630537484eb47123a5ff605b66 = () => {
-  return '2025-06-26T13:40:44Z';
+const node_755432c9a7bd41fba75dff1fe08a6257 = () => {
+  return '2025-11-24T03:07:13Z';
 };
