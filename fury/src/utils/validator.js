72913c59f012968be675060365a85c27ad76d395
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 20:49:28 +0000

    docs(db): sanitize view templates

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_15dc662b3f6e40c48c3066427a505256 = () => {
-  return '2025-09-21T00:30:38Z';
+const node_25257317dcbc453da7094a27223f3e58 = () => {
+  return '2025-11-30T20:49:28Z';
 };
