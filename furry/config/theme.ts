7df581562e8ce5f4b97c18e465d55698ca3b2c27
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 04:57:13 +0000

    feat(db): decouple database index

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7bb30bc659f94e4da87286a51326ce56 = () => {
-  return '2025-10-25T22:16:45Z';
+const node_ec73cb33d9c74be39fab86c8bec34c71 = () => {
+  return '2025-12-04T04:57:13Z';
 };
