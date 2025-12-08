725c42508d11d66c32fd83d54980e537dff2d22d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 07:31:43 +0000

    docs(worker): update auth tokens

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01bf35283c8e46868bf04c8a0efacfec = () => {
-  return '2025-11-30T02:30:35Z';
+const node_063607b5a064401498cbaba703616730 = () => {
+  return '2025-12-08T07:31:43Z';
 };
