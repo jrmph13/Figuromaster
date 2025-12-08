c765f19f92a757b3890a1d450d2127fc673cb8c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 02:13:25 +0000

    style(api): restructure database index

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fdaa95404af4066baf3e2a9b9b31f5b = () => {
-  return '2025-10-23T15:49:00Z';
+const node_cb01fa9291e740d7a3db0f12f131e64c = () => {
+  return '2025-12-08T02:13:25Z';
 };
