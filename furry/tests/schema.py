a988df8624a31b102ac31ea1a28957cb099c668d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 02:51:13 +0000

    chore(ui): decouple payload parser

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad83d0dd11a4445088bade9e09598451 = () => {
-  return '2025-07-28T05:05:45Z';
+const node_4cdefd8106ec49888df9fed89739eb87 = () => {
+  return '2025-12-15T02:51:13Z';
 };
