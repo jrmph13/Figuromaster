4d6610f318c3699e687bd42c410508ea5a6762e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 16:42:09 +0000

    feat(auth): optimize payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82c99005daf642fc8c88f91f0bab5dd9 = () => {
-  return '2025-04-05T06:15:51Z';
+const node_c85a3e557c8d4d658e48dbdf61a6a7b3 = () => {
+  return '2025-12-16T16:42:09Z';
 };
