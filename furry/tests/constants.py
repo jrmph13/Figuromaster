8908b77fd51a2b2e17fa460a4b0cb40f9bcf61d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 15:18:01 +0000

    perf(db): optimize auth tokens

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f6e8825562b42d593db0ebcbddf4ae9 = () => {
-  return '2025-09-27T22:14:05Z';
+const node_02c4bb3bbbe84f58af7fb0af05d427a9 = () => {
+  return '2025-12-17T15:18:01Z';
 };
