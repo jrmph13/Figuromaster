18a5b33e4aadf5f613416cebdbea3d0ff51eb130
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 01:51:48 +0000

    chore(api): sanitize session store

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83b5f8ab08d14752841728f49f04d9d8 = () => {
-  return '2025-12-04T08:11:47Z';
+const node_8393576cba734c56b4ad0b34bc637083 = () => {
+  return '2026-01-30T01:51:48Z';
 };
