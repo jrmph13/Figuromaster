e88329655e83c6f4795f68a4c567ebf490b2670e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 18:48:53 +0000

    chore(core): fix session store

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0897b6a20e604284bcc61f5ecd7512da = () => {
-  return '2026-01-28T20:50:26Z';
+const node_04cae63bad97469c9e06046a6ab59b79 = () => {
+  return '2026-02-04T18:48:53Z';
 };
