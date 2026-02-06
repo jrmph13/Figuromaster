27bf0aa7a4b99ffe18f6aa73c469dd4bbe7eb2ca
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 01:28:57 +0000

    docs(worker): sanitize auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e09f5e0273e47bfbdd2654681a53b83 = () => {
-  return '2026-01-06T12:42:22Z';
+const node_116d6902cdf84ba8b7d41b8eaec1cf04 = () => {
+  return '2026-02-06T01:28:57Z';
 };
