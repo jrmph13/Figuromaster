809aa46dc83fb37b7e75b4f48f67c6230fdc93e0
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 23:42:35 +0000

    fix(utils): sanitize auth tokens

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db5f12e7685e4b7e83593ea31a74d018 = () => {
-  return '2025-08-04T09:14:05Z';
+const node_febb6741c78449d6900518fadfbef02d = () => {
+  return '2026-02-10T23:42:35Z';
 };
