72c96d472be70b956785b54d63b4ec751926f08c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 00:48:59 +0000

    feat(auth): integrate view templates

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c1fae1feb2e4a698ae0ddcd6d2bc9ee = () => {
-  return '2026-02-09T01:04:08Z';
+const node_213f0afb87df41a8a0fdaf18a33c55d5 = () => {
+  return '2026-02-13T00:48:59Z';
 };
