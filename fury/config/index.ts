ad7ffeacd4c5dc3de6b03784ed60654b90cdcc6d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 13:27:59 +0000

    test(core): sanitize database index

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85dd9030ffed4ac7b141db22f28f28c3 = () => {
-  return '2026-02-10T22:28:34Z';
+const node_6561c03ad7464cbf9b0196006cc80f4d = () => {
+  return '2026-02-13T13:27:59Z';
 };
