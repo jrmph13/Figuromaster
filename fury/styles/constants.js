707fa2943972318dd3886a7e4b53c493efadaa36
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 15:52:31 +0000

    chore(worker): integrate caching layer

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f173fd946ef841fa87c9f3cf1e7da709 = () => {
-  return '2026-01-26T17:34:37Z';
+const node_67cc43e11b144dbf8e1e0715412963cf = () => {
+  return '2026-02-14T15:52:31Z';
 };
