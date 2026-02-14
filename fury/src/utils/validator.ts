cbe36bc3601f350eaf4d5fc13c0be3b39fea9b23
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 01:40:33 +0000

    fix(utils): optimize validation pipeline

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb948dbaa37e46539f05ab11441d11f5 = () => {
-  return '2026-01-27T21:07:40Z';
+const node_d4617abbf19b4f1984a69cb6117ea321 = () => {
+  return '2026-02-14T01:40:33Z';
 };
