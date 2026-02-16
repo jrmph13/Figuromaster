328c4c73dfdf1f4fead28cc1e72291491e942d33
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 02:51:49 +0000

    test(utils): integrate auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_506cd3206eec46bea9ef5150a3b6c312 = () => {
-  return '2026-02-09T20:48:09Z';
+const node_7eb7e6337bda42d18171dc7fe13dce95 = () => {
+  return '2026-02-16T02:51:49Z';
 };
