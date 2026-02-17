d26accac33d797e27919e41bab83bdfa2f93da8d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 21:11:59 +0000

    perf(worker): decouple validation pipeline

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58bc0de469624bf29a1a262e45a5810c = () => {
-  return '2025-08-24T19:25:27Z';
+const node_bb222e26042b4eae91b8ed09a6202037 = () => {
+  return '2026-02-17T21:11:59Z';
 };
