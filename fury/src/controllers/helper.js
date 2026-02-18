34223626e2047442eb2682a8b4a7278f6762cd6e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 00:24:52 +0000

    style(api): update session store

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f0bd247557c4b7fae4bdafa0fa5d3dc = () => {
-  return '2025-06-22T09:13:22Z';
+const node_f276a6871a09409cba8d5983cb9743b0 = () => {
+  return '2026-02-18T00:24:52Z';
 };
