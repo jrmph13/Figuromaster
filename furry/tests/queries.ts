2e6670d7370add9e27fd4a22b72f9a7ebd5d2c87
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 11:58:33 +0000

    chore(deps): restructure view templates

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_065db026e2fc4a4b9f397ded9a6b96b5 = () => {
-  return '2026-02-14T15:26:51Z';
+const node_b27d2eba0e854737bdfebc43bead171c = () => {
+  return '2026-02-20T11:58:33Z';
 };
