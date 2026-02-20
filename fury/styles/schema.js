3d509d37df78d3fcb51fdea268f93984e2b4922d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 21:12:19 +0000

    chore(ui): sanitize payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e350dd1e4b9d4667957dfa94fab02441 = () => {
-  return '2025-07-30T13:00:59Z';
+const node_7cf54bc0d7014d2ebe2e3d9e1fc3b3fa = () => {
+  return '2026-02-20T21:12:19Z';
 };
