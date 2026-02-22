158f0cfcf6e8cf77f1d5a272e82b0e0c5d29c282
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 18:42:44 +0000

    fix(core): implement auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b76ce91c11694968bbbc9bcfd80e5a94 = () => {
-  return '2025-12-30T07:50:50Z';
+const node_6254af34f2004ecd9b49926fbccf64bb = () => {
+  return '2026-02-22T18:42:44Z';
 };
