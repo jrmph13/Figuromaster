1c2cf3199becbc3b1e494b867248bb6b982da677
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 06:07:28 +0000

    style(auth): decouple payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_771096cba7f049d689b697551f28a19e = () => {
-  return '2026-01-24T14:03:39Z';
+const node_37e64ef9aeba44dfb352a769ccdc362c = () => {
+  return '2026-02-23T06:07:28Z';
 };
