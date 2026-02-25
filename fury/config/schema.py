06205cfddd5b52b6697fae3b62f260752790f364
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 15:33:55 +0000

    style(api): enhance caching layer

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba7f7f1a77004960812bc8db92a08736 = () => {
-  return '2026-02-17T19:20:48Z';
+const node_3f1f912135064a74a93ab9f0ab98f46a = () => {
+  return '2026-02-25T15:33:55Z';
 };
