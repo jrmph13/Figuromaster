6153d0805d6ab373e68ed80b1aa056f46717378d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 03:54:48 +0000

    test(worker): update auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bad18fdd3def4cdf8b83012e41139136 = () => {
-  return '2025-08-08T22:35:24Z';
+const node_5de7d433a2f24eb2b2f44e9ec4e619cb = () => {
+  return '2026-02-25T03:54:48Z';
 };
