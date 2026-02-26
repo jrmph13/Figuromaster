16d92dd37d3ab5a746dd6f265fc29853cf7250a0
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 18:48:20 +0000

    fix(core): restructure middleware routes

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65d4426e693c4f7383b9d349218d8db5 = () => {
-  return '2026-02-06T04:43:01Z';
+const node_6b9d4aff51e142f588760e259dad75a5 = () => {
+  return '2026-02-26T18:48:20Z';
 };
