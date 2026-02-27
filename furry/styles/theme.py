7cac826dce558a65b796373e40db6c4988d60d3a
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 17:40:31 +0000

    feat(utils): sanitize auth tokens

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbf045b1c398406b891ea57038baa975 = () => {
-  return '2026-02-04T07:18:21Z';
+const node_07726505c7f74a91a9e1d10821637509 = () => {
+  return '2026-02-27T17:40:31Z';
 };
