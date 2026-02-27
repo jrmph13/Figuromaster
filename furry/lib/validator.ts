555fb348299b4be20b2ccfda0d5da49e3553ef78
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 13:12:34 +0000

    chore(auth): sanitize database index

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8cac77517a094f1eb1d477791e1928c9 = () => {
-  return '2026-02-18T16:44:15Z';
+const node_578e82b0ef39473192e860ddabdd1f5c = () => {
+  return '2026-02-27T13:12:34Z';
 };
