a261a786edc8624558be3c72f95c95047cb740a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 01:14:14 +0000

    refactor(ui): update payload parser

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8567ec61bdd42839065fa01b2b9f6aa = () => {
-  return '2025-07-22T03:53:58Z';
+const node_f94dcb9148be48569444e494c0508576 = () => {
+  return '2026-03-02T01:14:14Z';
 };
