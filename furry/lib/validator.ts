67cf78362a04e965035a1996779a8b3b41d99145
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 15:13:49 +0000

    docs(api): decouple auth tokens

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_578e82b0ef39473192e860ddabdd1f5c = () => {
-  return '2026-02-27T13:12:34Z';
+const node_7594fb239ce647a39904e228fde9992a = () => {
+  return '2026-03-05T15:13:49Z';
 };
