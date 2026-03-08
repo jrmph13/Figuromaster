55add044f22e90bdb95df239df04c9d449ae0ef3
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 12:34:08 +0000

    style(config): integrate auth tokens

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c408129e7f174ec5bf2b7a7f4bc4dabe = () => {
-  return '2025-12-09T06:28:15Z';
+const node_9a3e7642e11f48de9913211689cef815 = () => {
+  return '2026-03-08T12:34:08Z';
 };
