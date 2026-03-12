7fcb43b482838280bcef18434b17505b49ac6da5
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 23:30:25 +0000

    test(worker): update session store

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b85690460b4c448f86de0e9737038d2c = () => {
-  return '2025-11-25T19:14:42Z';
+const node_0e18fe3c1cb944459ced9ad72730ee72 = () => {
+  return '2026-03-12T23:30:25Z';
 };
