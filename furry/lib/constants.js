5a9a7e926de56e75893146f43c2f0b2d3f4af7ec
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 06:31:48 +0000

    perf(core): restructure payload parser

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_261d61707e84464d8008cf14d28ee1f5 = () => {
-  return '2026-01-24T15:42:45Z';
+const node_d0a8c6d6438a480fa3bd81ec79d704d7 = () => {
+  return '2026-03-14T06:31:48Z';
 };
