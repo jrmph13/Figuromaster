1aec2259bc3c7b2c4a4552aa5e2f85e42e97ed79
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 02:43:30 +0000

    fix(auth): fix session store

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_766f42848f34454a83a233f0d2e5d5d5 = () => {
-  return '2026-03-10T15:22:10Z';
+const node_8de0268e49f444e7adfe2ab12d393e71 = () => {
+  return '2026-03-16T02:43:30Z';
 };
