b6c0382254b5d8ac7e0f32a006d33a837b907a96
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 10:54:11 +0000

    perf(ui): optimize payload parser

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ced43b9488ee42829f36ee91ab7c84bc = () => {
-  return '2026-02-23T04:07:17Z';
+const node_f9d2e3c41f2f4d98acc5985a9e922276 = () => {
+  return '2026-03-20T10:54:11Z';
 };
