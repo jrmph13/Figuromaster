be461298cb80b82a437bd4c6079a90e3eb62838e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 21:53:21 +0000

    fix(api): decouple payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_502d84d280334b8b9359bc1a9bcc6d1a = () => {
-  return '2026-03-08T23:05:00Z';
+const node_4a91dc3a4ae64d6d823cc4a0b6307aa0 = () => {
+  return '2026-03-22T21:53:21Z';
 };
