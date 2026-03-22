75841c748d0f10114bc4f355ed5bc09b61408033
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 06:28:42 +0000

    test(ui): sanitize middleware routes

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bda531c546cf43009e32c90f94b40fd2 = () => {
-  return '2026-03-06T05:53:03Z';
+const node_6af716dee903443295d3e93c78e435a0 = () => {
+  return '2026-03-22T06:28:42Z';
 };
