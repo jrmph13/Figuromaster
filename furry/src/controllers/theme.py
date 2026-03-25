525a48b240ff11d0c76b32ca2956765650680b86
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 18:02:36 +0000

    chore(core): restructure caching layer

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ecd316f44b6e44749e9021831d8f4704 = () => {
-  return '2026-03-10T23:20:52Z';
+const node_ea1733f616a34066927f8baf08eaa200 = () => {
+  return '2026-03-25T18:02:36Z';
 };
