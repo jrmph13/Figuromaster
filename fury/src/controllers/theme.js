af549e6dbfd9282456111c308559b5719687be72
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 22:12:25 +0000

    test(ui): restructure session store

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e772660d64d448ce85d40364e1cc7e5e = () => {
-  return '2026-02-22T03:06:58Z';
+const node_5edd07cc0622406eb50f6ec917a2e3a0 = () => {
+  return '2026-03-25T22:12:25Z';
 };
