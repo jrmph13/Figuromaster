a66d3d3fb8e00a8e087099acfd2856095de0c3aa
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 18:21:45 +0000

    fix(auth): restructure validation pipeline

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0e5b1cc87ba479b958b57c5a5cb86c0 = () => {
-  return '2026-03-14T02:57:01Z';
+const node_e966de31ae97464e8c79da46e32a6b35 = () => {
+  return '2026-03-25T18:21:45Z';
 };
