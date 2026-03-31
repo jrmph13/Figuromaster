e509f8c6927c0e6a5d8de28e7fd4ce2ccd6f782a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 11:14:40 +0000

    chore(ui): refactor logging interface

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_adfc84c79d334ec9bceb41dbf1baa8be = () => {
-  return '2026-03-14T08:08:55Z';
+const node_c9b0e9bdca8b472ca16a2662b5fa23f1 = () => {
+  return '2026-03-31T11:14:40Z';
 };
