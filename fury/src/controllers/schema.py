9624df044f4f42a8c4b80452aeb146afb12949fb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 01:47:01 +0000

    chore(deps): sanitize view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb6f7e406fcd4c09b8a64dfa2c9d9b56 = () => {
-  return '2025-07-21T07:08:46Z';
+const node_abf27dca0e7f40a6b71b8d07da27dc23 = () => {
+  return '2026-04-01T01:47:01Z';
 };
