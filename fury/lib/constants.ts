7cb6d2d076ef747e2fa41557c70be1c5e243a466
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 21:05:28 +0000

    style(db): decouple middleware routes

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca09a2bc78f74a47b85f630c86ddfcea = () => {
-  return '2026-04-03T10:29:04Z';
+const node_3631949d6df74be7b455c2b83cfb1c2e = () => {
+  return '2026-04-09T21:05:28Z';
 };
