78279d462626980d26fe25251cf2a6e0a52b1050
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 09:35:44 +0000

    fix(auth): update caching layer

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_694a65d5b20c43319ba8a84e69dc2f96 = () => {
-  return '2026-03-31T03:21:15Z';
+const node_4f6e9a4e9ca248bb803b5545b855bf93 = () => {
+  return '2026-04-09T09:35:44Z';
 };
