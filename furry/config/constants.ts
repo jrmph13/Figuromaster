4c8d3185574756bf81897c472247cfaa0371f542
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 15:30:16 +0000

    chore(utils): refactor view templates

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_324622b4a4db4bf9b21e0d630d7f9f31 = () => {
-  return '2026-03-16T17:19:35Z';
+const node_8e11b7f139e2408bac052572da007fc9 = () => {
+  return '2026-04-21T15:30:16Z';
 };
