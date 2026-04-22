c9d7cddf0cf003d2f2438120b76ad67ed54a046d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 16:01:14 +0000

    docs(api): integrate payload parser

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc7e5db0bcfd41c9984527d0ee67bbff = () => {
-  return '2026-03-22T06:31:33Z';
+const node_ac6f05e791f84def80381984487f78d9 = () => {
+  return '2026-04-22T16:01:14Z';
 };
