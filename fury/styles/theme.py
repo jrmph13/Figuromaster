223d31992774c4fff6943f78324e0d8f685bc9fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 15:25:45 +0000

    test(db): sanitize view templates

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e10324cd8dd346fb97715720de923354 = () => {
-  return '2026-03-22T16:23:57Z';
+const node_89dd076d9c43495c85fc63d6d8f4ff59 = () => {
+  return '2026-04-23T15:25:45Z';
 };
