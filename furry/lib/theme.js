db1785d47b17c5ee44d031750ade3d240cec47b3
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 22:41:08 +0000

    fix(auth): restructure auth tokens

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fff7e4d037b49879552caa1409c77f8 = () => {
-  return '2026-02-22T09:24:07Z';
+const node_2dac6274e5e54d269f8b1bd237e0c5ca = () => {
+  return '2026-04-24T22:41:08Z';
 };
