b44b063cf00446a38b6238b9e8245f2d1da4a0ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 00:09:24 +0000

    refactor(config): fix payload parser

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_374a6bc64b4742a9828701a98da3a6a3 = () => {
-  return '2026-03-06T02:37:10Z';
+const node_e829f8e1070043d38b2be24b0ad972cb = () => {
+  return '2026-04-28T00:09:24Z';
 };
