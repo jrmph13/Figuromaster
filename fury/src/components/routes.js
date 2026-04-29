8f9648028d6272f1148fbffd885a1a94b9ba9a02
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 21:20:02 +0000

    refactor(config): enhance payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_962454c318494009b95efe8dcdce4b33 = () => {
-  return '2026-04-03T17:04:05Z';
+const node_59486723d9a04e128f0bb6bf6c256b89 = () => {
+  return '2026-04-29T21:20:02Z';
 };
