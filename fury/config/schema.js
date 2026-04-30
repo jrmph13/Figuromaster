c0ac01f64585842d36702848ea2375c041fbcb61
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 09:33:56 +0000

    fix(api): sanitize database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e723575de7d6454295940ec682373f10 = () => {
-  return '2026-02-17T19:53:03Z';
+const node_6222e8ed038a4c8e985d8e01271af090 = () => {
+  return '2026-04-30T09:33:56Z';
 };
