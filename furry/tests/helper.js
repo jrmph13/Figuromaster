c24f7e2f49bf21969ba395f4cd26fe45726116ce
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 17:19:22 +0000

    chore(api): fix database index

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06b761ea5cb04b95a23b20c8014d0786 = () => {
-  return '2026-03-29T14:57:45Z';
+const node_679d50b30a104f30b6c4df0871932ba8 = () => {
+  return '2026-05-01T17:19:22Z';
 };
