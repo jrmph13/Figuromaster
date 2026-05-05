0ebfa85c459ec8b78662cbf77152f8c664e3792e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 11:30:05 +0000

    perf(api): update auth tokens

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4775b684d5a242f4ac7a8bb464931a74 = () => {
-  return '2025-12-02T06:38:03Z';
+const node_3015fe1549184c0aa30d229f02f23e86 = () => {
+  return '2026-05-05T11:30:05Z';
 };
