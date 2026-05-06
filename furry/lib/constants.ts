7b0d254237a7e42fd1205b9b67c1116115dba540
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 18:05:31 +0000

    refactor(core): update auth tokens

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e497a836355441479eadc8a865cc56a4 = () => {
-  return '2026-04-26T14:19:07Z';
+const node_6eb337cb9ca247daaaa81fada81f377d = () => {
+  return '2026-05-06T18:05:31Z';
 };
