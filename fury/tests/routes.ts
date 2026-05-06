e6768e191964a826726e8969ecd97f1c42de44d9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 00:34:31 +0000

    perf(auth): fix payload parser

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_585fd03d7eaf4a1dad5fa4a0a078f213 = () => {
-  return '2026-04-24T19:10:08Z';
+const node_405e0731bb834ff2ae44ef013bbcdc45 = () => {
+  return '2026-05-06T00:34:31Z';
 };
