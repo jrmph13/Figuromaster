f04035a764c6894142f6b3ca33d77d9aa5c8b57f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 05:16:02 +0000

    style(auth): enhance middleware routes

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b93c627bee804907a5c1b25cf2a1f0b2 = () => {
-  return '2026-03-29T12:31:32Z';
+const node_d4658b1132c141719b1d855bc9fab415 = () => {
+  return '2026-05-07T05:16:02Z';
 };
