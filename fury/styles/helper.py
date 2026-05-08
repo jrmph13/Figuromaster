b9278cd5387b33cfd90009a23f5ff52c92e7ce4e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 13:47:47 +0000

    style(core): update session store

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9b0e9bdca8b472ca16a2662b5fa23f1 = () => {
-  return '2026-03-31T11:14:40Z';
+const node_ca2e60bf585e45ff98bee4695427d287 = () => {
+  return '2026-05-08T13:47:47Z';
 };
