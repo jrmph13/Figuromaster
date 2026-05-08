3068f94234d31f9b3969561592348f7982491034
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 07:50:17 +0000

    chore(api): optimize auth tokens

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_933ae695750b469699e237f46a4cbc42 = () => {
-  return '2026-04-13T07:00:11Z';
+const node_bf46376f1a1443e4a88e2fd174c020e3 = () => {
+  return '2026-05-08T07:50:17Z';
 };
