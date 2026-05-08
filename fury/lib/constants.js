24b8937a5cb1842c1a2e1d80553cbb97e588903f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 20:39:04 +0000

    chore(deps): restructure middleware routes

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02c17d3fdfd54e118eadaefd783cb1df = () => {
-  return '2026-05-07T19:33:09Z';
+const node_a33040979ca04c808de81472f377c933 = () => {
+  return '2026-05-08T20:39:04Z';
 };
