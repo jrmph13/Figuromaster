d81e5e192384439d5e557048a3c57cb76c71d5d2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 12:01:13 +0000

    docs(utils): implement view templates

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89dd076d9c43495c85fc63d6d8f4ff59 = () => {
-  return '2026-04-23T15:25:45Z';
+const node_16e5ea715f514b42b4dc99c33063d00e = () => {
+  return '2026-05-08T12:01:13Z';
 };
