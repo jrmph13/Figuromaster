12d44135e87ae731e1a14235d7809d0f1ea26002
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 03:31:21 +0000

    test(auth): fix session store

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea33ab8338be4d0e90677bd6c40ab773 = () => {
-  return '2026-05-08T23:05:27Z';
+const node_bb6f947807aa4015a8c0809718d92002 = () => {
+  return '2026-05-11T03:31:21Z';
 };
