636277b2fafdb2d79748e59dbd94e6054b2ddfad
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 17:13:34 +0000

    perf(core): update session store

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eef62670d94d4c179abe695f231c5a1b = () => {
-  return '2026-05-02T03:58:32Z';
+const node_e43b14ba9348434586de4985c1607ccf = () => {
+  return '2026-05-13T17:13:34Z';
 };
