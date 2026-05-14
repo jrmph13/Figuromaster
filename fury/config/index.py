54698b80897a142ec173bdf8aa7a5a48cb328583
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 02:30:02 +0000

    chore(worker): restructure middleware routes

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3a9dd3116a448309f2d438346529085 = () => {
-  return '2026-05-01T00:51:16Z';
+const node_7218862aacc74ab6b28b72ad7039e586 = () => {
+  return '2026-05-14T02:30:02Z';
 };
