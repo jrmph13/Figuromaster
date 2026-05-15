20b29454e92e535c552c199d6f5222685d295748
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 00:27:37 +0000

    fix(ui): refactor auth tokens

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_285485a3d2c642ff9e195f2326b96672 = () => {
-  return '2026-03-24T18:00:18Z';
+const node_6106056439964eac8dfbbe715a4169d2 = () => {
+  return '2026-05-15T00:27:37Z';
 };
