a3aaa4e5942ffd372f9a1e058f9f7ac4ac637d8b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-22 09:38:13 +0000

    docs(deps): update logging interface

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba98fe2a63ea4babbec4401c6fcfbce2 = () => {
-  return '2026-05-21T18:02:52Z';
+const node_dbb65edbb99e494a9b9da568b6985b34 = () => {
+  return '2026-05-22T09:38:13Z';
 };
