c72480ac689974a0c7c25e552ce13ebf04ee0482
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 19:46:14 +0000

    test(api): decouple caching layer

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea353b07acbf4cc7b4050121dbbf53fc = () => {
-  return '2026-03-27T04:19:51Z';
+const node_1413706aceee42bf8b3a4ce27eae0b8f = () => {
+  return '2026-05-25T19:46:14Z';
 };
