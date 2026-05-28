6128fc7aeaea8310154c488141d3fb2936f614e9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 22:45:45 +0000

    style(utils): implement auth tokens

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_548ba1ba3bd440c6a9feac22e0018db5 = () => {
-  return '2026-05-11T00:12:39Z';
+const node_153fa32cfd2648b3b3d662642c42b918 = () => {
+  return '2026-05-28T22:45:45Z';
 };
