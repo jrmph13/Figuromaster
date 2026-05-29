d02ce634290522a82612470e6975b99b4b83604d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 20:47:45 +0000

    fix(ui): integrate view templates

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b63351f929e24389b9300a87c5838cf7 = () => {
-  return '2026-04-26T00:32:06Z';
+const node_cc102d2247984b8a96f1e5257074df97 = () => {
+  return '2026-05-29T20:47:45Z';
 };
