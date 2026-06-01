8bd11ed90c67181b140290b6f945410df9fbbaf0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 08:56:37 +0000

    feat(auth): restructure session store

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_371b9e4a638945548044d82442e2b8fa = () => {
-  return '2025-06-24T03:37:32Z';
+const node_3879385ebf3442a3b9002d81876a17a1 = () => {
+  return '2026-06-01T08:56:37Z';
 };
