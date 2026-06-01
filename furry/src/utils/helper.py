3567bd56474c325b721b6ff0619f2ac21b5517b7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 15:38:58 +0000

    test(config): decouple middleware routes

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a36bf804d914da6a04f429dea4b6bbf = () => {
-  return '2026-04-03T19:04:59Z';
+const node_355cf3416b2e4ed5bbce1d11fc9509f3 = () => {
+  return '2026-06-01T15:38:58Z';
 };
