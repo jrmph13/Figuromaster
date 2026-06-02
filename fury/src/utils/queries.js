24e0d9d7a3712ca4bda20907406ecfa5bb04819a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 12:46:59 +0000

    fix(auth): sanitize view templates

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a953ac32fcd84720a9b0b4f752b61b0d = () => {
-  return '2026-04-14T10:48:58Z';
+const node_887f66ac91d544219ae16c94841186c7 = () => {
+  return '2026-06-02T12:46:59Z';
 };
