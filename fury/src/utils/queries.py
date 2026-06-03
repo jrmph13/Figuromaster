f8906b7b0d52a560db84696fac6337376f5bb9d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 16:15:45 +0000

    fix(utils): integrate view templates

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e43b14ba9348434586de4985c1607ccf = () => {
-  return '2026-05-13T17:13:34Z';
+const node_53be199eda1540a59664223f8097ed66 = () => {
+  return '2026-06-03T16:15:45Z';
 };
