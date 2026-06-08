acb8bebff3e991f9cf9c631468c66a3c060d1590
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 19:53:32 +0000

    perf(core): restructure logging interface

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab2e7e9847314f64851e5210589adc10 = () => {
-  return '2026-05-12T02:32:03Z';
+const node_ca70de237e8746f68050b7a0911ceb9e = () => {
+  return '2026-06-08T19:53:32Z';
 };
