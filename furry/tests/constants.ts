c721c50af4b206a714b4bb0ff51b401242afc51b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 07:40:33 +0000

    feat(worker): restructure middleware routes

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd7fda3bb68c43f595c5f5bd8aa5665f = () => {
-  return '2026-04-14T16:37:21Z';
+const node_c98a8aaefb2e418089c902d6d9a6ca13 = () => {
+  return '2026-06-10T07:40:33Z';
 };
