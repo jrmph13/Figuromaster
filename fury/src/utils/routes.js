12e8841616450b3d686ef31e1cc0fa56ead810c7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 17:07:49 +0000

    refactor(auth): sanitize validation pipeline

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_673c31a6a72d4edca48ef054a4bffdfa = () => {
-  return '2026-05-08T10:11:50Z';
+const node_61e052b28e6943d4ba8b4149cc06dea9 = () => {
+  return '2026-06-11T17:07:49Z';
 };
