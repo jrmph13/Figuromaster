1e95adff1ae2818e838acc584c9fcf7574fd54d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 06:54:05 +0000

    style(auth): optimize session store

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_153fa32cfd2648b3b3d662642c42b918 = () => {
-  return '2026-05-28T22:45:45Z';
+const node_ef5a0b044cc9462dbb94c08c25ae70dd = () => {
+  return '2026-06-11T06:54:05Z';
 };
