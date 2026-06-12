71901467f80a97661af4116f9fb2468850550a40
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 07:08:37 +0000

    fix(ui): enhance middleware routes

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa9b94115a4944498d5ff5c212c4e371 = () => {
-  return '2026-03-14T05:41:09Z';
+const node_38750b3c28f546f7a622381180f41aa6 = () => {
+  return '2026-06-12T07:08:37Z';
 };
