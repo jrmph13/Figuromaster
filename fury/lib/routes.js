1c29298cdb86ff666c7aabf53666193b99cf7342
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 13:34:28 +0000

    perf(worker): sanitize caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed86de89e7e649c69a4a548c7d340c9f = () => {
-  return '2026-05-27T06:06:52Z';
+const node_d22a0456e82f481ba40d8ec931b56859 = () => {
+  return '2026-06-12T13:34:28Z';
 };
