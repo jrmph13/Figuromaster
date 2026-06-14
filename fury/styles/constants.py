b69607678f73f1bccab069dae91414a98a3dd519
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 17:40:17 +0000

    style(utils): fix middleware routes

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e51cc6d36ff473e9c3521c6d57c7581 = () => {
-  return '2026-05-25T00:33:23Z';
+const node_a4f6ca20dab0492ba6626b2108d6a496 = () => {
+  return '2026-06-14T17:40:17Z';
 };
