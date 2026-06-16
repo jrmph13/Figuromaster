d25bc7c30e17a13eebc24ad50e897aa2799c3123
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 22:15:07 +0000

    refactor(ui): integrate caching layer

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9d2e3c41f2f4d98acc5985a9e922276 = () => {
-  return '2026-03-20T10:54:11Z';
+const node_7e8cc9bb908b4ecf94b230391d93f3ae = () => {
+  return '2026-06-16T22:15:07Z';
 };
