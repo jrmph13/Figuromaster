ef260e37fd48ff1a321fdf451698bce829259b2e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 23:28:30 +0000

    test(api): enhance logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f22bc40087384701b1cde361f60e3280 = () => {
-  return '2026-05-26T12:50:14Z';
+const node_ada8abf39c89488ebc533b3ed8b4db29 = () => {
+  return '2026-06-20T23:28:30Z';
 };
