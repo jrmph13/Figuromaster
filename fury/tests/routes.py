4fcc6b279ff0f989e94116ef71419e08ca6c22a5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 07:08:10 +0000

    test(db): sanitize view templates

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb6e566384b24821b097fe08b2ec5475 = () => {
-  return '2026-04-02T06:41:23Z';
+const node_79623c97b4cf4e6d80ff587d42fb68ef = () => {
+  return '2026-06-20T07:08:10Z';
 };
