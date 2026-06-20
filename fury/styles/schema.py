3636badceb25c2352c7651edf5bd8bb9d9034587
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 15:41:46 +0000

    refactor(core): sanitize auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18d350a7f2df42aea750669a5a97ec0a = () => {
-  return '2026-05-10T03:54:45Z';
+const node_d3b6737131004d6dade8299dca662ff1 = () => {
+  return '2026-06-20T15:41:46Z';
 };
