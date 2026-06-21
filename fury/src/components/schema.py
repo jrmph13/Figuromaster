2ae84ad90730b3138f8401d0d32a9db78e289906
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 12:22:40 +0000

    chore(utils): decouple logging interface

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71944afa6eb74aaa9e9dadf77785275c = () => {
-  return '2026-06-18T18:16:38Z';
+const node_8489f78eac464433b90e06445bd6124a = () => {
+  return '2026-06-21T12:22:40Z';
 };
