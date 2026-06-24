61ab51b16529c31500cba978f0601e02be65a359
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 16:32:56 +0000

    test(deps): restructure middleware routes

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4f6ca20dab0492ba6626b2108d6a496 = () => {
-  return '2026-06-14T17:40:17Z';
+const node_5016386e5955477c8ef6ddc02bc64401 = () => {
+  return '2026-06-24T16:32:56Z';
 };
